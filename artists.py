4887537caab4359fe375f100d7e8077de6efa723
Devin Etcitty <devin.etcitty@gmail.com>
2017-04-18 16:38:41 -0400

    Prototype of csv maker file

--- artists.py
+++ artists.py
@@ -8,8 +8,21 @@ Jacob Hofman
 source: https://github.com/plamere/spotipy
 '''
 import spotipy
-sp = spotipy.Spotify()
+import csv
+import sys
 
-results = sp.search(q='weezer', limit=20)
-for i, t in enumerate(results['tracks']['items']):
-        print(' ', i, t['name'])
+sp = spotipy.Spotify()
+limit = 20
+
+
+if len(sys.argv) > 1:
+    csv_file = sys.argv[1]
+
+with open(csv_file, newline='\n') as csvfile:
+    reader = csv.reader(csvfile, delimiter=' ')
+    for row in reader:
+        artist = row[0]
+        results = sp.search(artist, limit)
+        for i, t in enumerate(results['tracks']['items']):
+            print(' ', i, t['name'])
+
