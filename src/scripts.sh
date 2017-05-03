f790d177b2f1fa26b3e1f6857c8513a5cb267505
as5194#columbia.edu <as5194@columbia.edu>
2017-05-02 21:06:59 -0400

    fetch URIs for songs

--- src/scripts.sh
+++ src/scripts.sh
@@ -1,3 +1,5 @@
-#~/bin/bash
-cut -d'|' -f1 us_billboard.psv | sort -nr | uniq -c > this_week_position_distribution.txt
-cut -d'|' -f5 us_billboard.psv | sort -nr | uniq -c | sort -nr > song_counts.txt
+#!/bin/bash
+file_name="../data/us_billboard_25.psv"
+cut -d'|' -f1 $file_name | sort -nr | uniq -c > this_week_position_distribution.txt
+cut -d'|' -f5 $file_name | sort -nr | uniq -c | sort -nr > song_counts.txt
+cut -d'|' -f5 $file_name | sort -nr | uniq > song_names.txt
