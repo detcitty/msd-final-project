4fe2b3cb5e1125c398f55bb5c79b5f550b99fa9b
Devin Etcitty <devin.etcitty@gmail.com>
2017-05-04 10:49:19 -0400

    Add lyrics

--- lyric/get_artist.py
+++ lyric/get_artist.py
@@ -1,6 +1,24 @@
 from musixmatch import matcher
+import csv
 
-chart = matcher.track(q_track="LoveGame", q_artist="Lady Gaga")
+data = "./../data/"
+billboard = "us_billboard.psv"
+billboard_25 = "us_billboard_25.psv"
+
+file_open = data + billboard
+
+with open(file_open) as f:
+    reader = csv.reader(f, delimiter="|")
+    file_of_data= [line for line in reader]
+
+    for line in file_of_data:
+        track = line[4]
+        artist = line[5]
+
+        print(track + " " + artist )
+
+
+#chart = matcher.track(q_track="LoveGame", q_artist="Lady Gaga")
 #print(chart)
-ly = chart.lyrics()
-print(ly['lyrics_body'])
+#ly = chart.lyrics()
+#print(ly['lyrics_body'])
