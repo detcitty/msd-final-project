14c83965afb3c4f2f3cb183225d65c039db00673
Fiona Rowan <fmr2112@columbia.edu>
2017-05-04 01:48:36 -0400

    finished script to get all audio features for given songs

--- src/audio_features.py
+++ src/audio_features.py
@@ -20,31 +20,69 @@ infile.close()
 
 tids = []
 
+# write to this file
+outfile = open("audio_features.txt", "w") 
+
+
 # first two uris, fetch uris
-for i in range(2):
+for i in range(len(data)):
     track_uri = data[i].split("|")[1].split(":")[-1]
-    track_uri = unicode(("spotify:track:" + track_uri).strip())
+    track_uri = unicode(("spotify:track:" + track_uri).strip('\n'))
     print(track_uri)
     tids.append(track_uri)
+    print("line ",str(i) )
 #    print(tids)
-#    feature = sp.audio_features(str(track_uri))[0]
-#    print(feature)
-#    raw_input()
+    feature = sp.audio_features(str(track_uri))[0]
+
+    feature_elements = [feature["track_href"],
+			feature["analysis_url"],
+			feature["energy"],
+			feature["liveness"],
+			feature["tempo"],
+			feature["speechiness"],
+			feature["uri"],
+			feature["acousticness"],
+			feature["instrumentalness"],
+			feature["time_signature"],
+			feature["danceability"],
+			feature["key"],
+			feature["duration_ms"],
+			feature["loudness"],
+			feature["valence"],
+			feature["type"],
+			feature["id"],
+			feature["mode"]]
+    for e in range(len(feature_elements)):
+        feature_elements[e] = str(feature_elements[e])
+    feature_string = "|".join(feature_elements)
+    feature_string += "\n"
+    outfile.write(feature_string) 
+    
+    print(feature_string)
+    
+
+
+# raw_input()
+outfile.close()
 
 #features = sp.audio_features(tids)
 #for f in features:
 #    print(f)
 
 start = time.time()
-features = sp.audio_features(tids)
-for feature in features:
-    print(feature)
-raw_input()
-delta = time.time() - start
-for feature in features:
-    print(json.dumps(feature, indent=4))
-    print()
-    analysis = sp._get(feature['analysis_url'])
-    print(json.dumps(analysis, indent=4))
-    print()
-print ("features retrieved in %.2f seconds" % (delta,))
+#features = sp.audio_features(tids)
+#for feature in features: 
+#    print(json.dumps(feature, indent=4))
+#    print()   
+#    analysis = sp._get(feature['analysis_url'])
+#    print(json.dumps(analysis, indent=4))
+    #print(feature)
+#raw_input()
+#delta = time.time() - start
+#for feature in features:
+#    print(json.dumps(feature, indent=4))
+#    print()
+#    analysis = sp._get(feature['analysis_url'])
+#    print(json.dumps(analysis, indent=4))
+#    print()
+#print ("features retrieved in %.2f seconds" % (delta,))
