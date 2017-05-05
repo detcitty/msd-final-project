a95e2ff7ace2daef708e4252c684e4782cb47204
Devin Etcitty <devin.etcitty@gmail.com>
2017-05-05 01:11:49 +0000

    Working on removing lines

--- lyric/remove_last_lines.py
+++ lyric/remove_last_lines.py
@@ -1,3 +1,20 @@
 import os
 
+path_name = "db_lyrics/"
 
+
+numline=3 #3 lines to skip
+
+for (dirpath, dirnames, filenames) in walk("."+path_name):
+        
+    with open(filenames,"w+") as o:
+
+    for i in range(numline):
+    f.next()
+    for line in f:
+        if p:
+            o.write(p)
+    p=line
+    f.close()
+    o.close()
+    print(os.listdir("."))
