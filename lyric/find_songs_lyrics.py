01fbf469ec9a37db2e3120fb15df7ab20c972190
Devin Etcitty <devin.etcitty@gmail.com>
2017-05-05 05:25:40 +0000

    Find lyrics for song

--- lyric/find_songs_lyrics.py
+++ lyric/find_songs_lyrics.py
@@ -1,3 +1,12 @@
 import os
+import glob
+import re
 
+path_name = "./db_lyrics/*.txt"                                                                                                                           
+output_path = "./output_lyrics/" 
+
+
+for f in glob.glob(path_name):
+    split_file = [a for a in re.split(r'([A-Z][a-z]*)', f) if a]
+    print(split_file)
 
--- lyric/remove_last_lines.py
+++ lyric/remove_last_lines.py
@@ -1,10 +1,11 @@
 import os
 import glob
+
+
 path_name = "./db_lyrics/*.txt"
 output_path = "./output_lyrics/"
+
 numline=3 #3 lines to skip
-
-
 
 for f in glob.glob(path_name):
     lines = open(f).readlines()
